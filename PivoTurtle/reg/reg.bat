da738807e8dd70c17c3f5deabb1aa18b306edfa9
Remigius Stalder <remigius.stalder@descom-consuting.ch>
2012-05-16 22:31:57 +0200

    fix registration and project settings

--- PivoTurtle/reg/reg.bat
+++ PivoTurtle/reg/reg.bat
@@ -3,7 +3,8 @@ set DOTNET_HOME=C:\Windows\Microsoft.NET\Framework64\v4.0.30319
 set BUILD=Debug
 #set BUILD=Release
 
-copy PivoTurtle-bare.reg PivoTurtle.reg
-%DOTNET_HOME%\RegAsm bin\%BUILD%\PivoTurtle.dll /codebase /regfile:PivoTurtle.reg
+%DOTNET_HOME%\RegAsm ..\bin\%BUILD%\PivoTurtle.dll /codebase /regfile:PivoTurtle.reg
+
+copy /b PivoTurtle.reg+PivoTurtle-bare.reg PivoTurtle-%BUILD%.reg
 
 pause

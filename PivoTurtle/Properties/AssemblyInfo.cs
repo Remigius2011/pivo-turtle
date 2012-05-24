06d66d79f62b77aa6ceea55226f5b46b44faf2d1
Remigius Stalder <remigius.stalder@descom-consulting.ch>
2012-05-24 14:47:08 +0200

    apply apache 2.0 license

--- PivoTurtle/Properties/AssemblyInfo.cs
+++ PivoTurtle/Properties/AssemblyInfo.cs
@@ -1,4 +1,20 @@
-﻿using System.Reflection;
+﻿/*
+ Copyright 2012 Descom Consulting Ltd.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -6,11 +22,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("PivoTurtle")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("TortoiseGit and TortoiseSvn Tracker Plugin for Pivotal Tracker (www.pivotaltracker.com)")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("Descom Consulting Ltd.")]
 [assembly: AssemblyProduct("PivoTurtle")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2012")]
+[assembly: AssemblyCopyright("Copyright © Descom Consulting Ltd. 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 

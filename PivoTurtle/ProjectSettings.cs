d301fcf678701c65436529b7dcf2de531b602717
Remigius Stalder <remigius.stalder@descom-consuting.ch>
2012-05-24 23:31:11 +0200

    add project settings

---
+++ PivoTurtle/ProjectSettings.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PivoTurtle
+{
+    public class ProjectSettings
+    {
+        public const string fileName = ".pivoturtle";
+
+        public const string keyMessageTemplate = "messageTemplate";
+        public const string keyEnforceMessage = "enforceMessage";
+        public const string keyEnforceStory = "enforceStory";
+
+        private Dictionary<string, string> properties = new Dictionary<string, string>();
+
+        public Dictionary<string, string> Properties
+        {
+            get { return properties; }
+        }
+
+        public void Save(string basePath)
+        {
+            string filePath = basePath + "\\" + fileName;
+        }
+
+        public void Load(string basePath)
+        {
+            string filePath = basePath + "\\" + fileName;
+        }
+    }
+}
